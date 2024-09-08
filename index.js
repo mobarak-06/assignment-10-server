6400fb87b215c53bae4246b42962b5d89413d48e
Mobarak Hossain <mobarakhossain121838@gmail.com>
2024-09-08 11:25:34 +0600

    post

--- index.js
+++ index.js
@@ -1,18 +1,61 @@
-const express = require('express');
-const cors = require('cors');
-require('dotenv').config()
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
 
 // middleware
 app.use(cors());
-app.use(express());
+app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.send('Hello From Backend Of Assignment 10')
-})
+const uri = `mongodb+srv://${process.env.USER_NAME}:${process.env.USER_PASS}@cluster0.dyqxkty.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+    const database = client.db("Arts&craftItemDB");
+    const craftCollection = database.collection("craftItem");
+
+    app.get("/allCraftItems", async(req, res) => {
+      const query = craftCollection.find();
+      const result = await query.toArray();
+      res.send(result);
+    })
+
+    app.post("/addCraftItem", async (req, res) => {
+      const users = req.body;
+      const result = await craftCollection.insertOne(users);
+      res.send(result);
+    });
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("Hello From Backend Of Assignment 10");
+});
 
 app.listen(port, () => {
-    console.log(`assignment 10 is running on port ${port} `);
-})
+  console.log(`assignment 10 is running on port ${port} `);
+});
